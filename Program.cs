729836481ee1a2f6748fdca5f22d82b8262527f6
funa4i <ne_kroman@DESKTOP-B0TJ7PH>
2024-07-06 21:54:38 +0400

    init commit

--- Program.cs
+++ Program.cs
@@ -8,13 +8,6 @@ namespace FlowerSellerTgBot
     {
         public static void Main(string[] args)
         {
-            string tocken;
-            // string tocken = Environment.GetEnvironmentVariable("TELEGRAM_TOCKEN_BOT");
-            using (StreamReader streamReader = new StreamReader("tocken.txt", Encoding.UTF8))
-            {
-              tocken = streamReader.ReadToEnd();
-            }
-            Console.WriteLine(tocken);
             Console.ReadLine();
         }
     }
