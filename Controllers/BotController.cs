04629e212d2600238c7f3a5e0e9529a639182c5c
funa4i <ne_kroman@DESKTOP-B0TJ7PH>
2024-07-08 17:15:03 +0400

    Внедрение зависимостей + вытгивание токена бота

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -9,17 +9,19 @@ namespace FlowerSellerTgBot.Controllers
     public class BotController : ControllerBase
     {
         private readonly ILogger<BotController> _logger;
-        public BotController(ILogger<BotController> logger) 
+        private readonly Telegram.Bot.ITelegramBotClient _bot;
+        public BotController(ILogger<BotController> logger, ITelegramBotClient bot) 
         {
             _logger = logger;
+            _bot = bot;
         }
-        private Telegram.Bot.TelegramBotClient bot = Bot.GetTelegramBotClient();
+        
 
         [HttpPost]
         public async void Post([FromBody] Update update)
         {
             long chatId = update.Message.Chat.Id;
-            await bot.SendTextMessageAsync(chatId, "Echo: " + update.Message.Text);
+            await _bot.SendTextMessageAsync(chatId, "Echo: " + update.Message.Text);
             _logger.LogInformation(update.Message.Chat.FirstName + " " + update.Message.Chat.LastName + " " + update.Message.Text);
         }
 
--- Program.cs
+++ Program.cs
@@ -1,17 +1,29 @@
+using Telegram.Bot;
+
 namespace FlowerSellerTgBot
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+          
             var builder = WebApplication.CreateBuilder(args);
+
+            
+            var token = builder.Configuration["Telegram:Token"] ?? throw new ArgumentNullException("Пустой токен");
+            
+            
+            builder.Services.AddHttpClient<ITelegramBotClient, TelegramBotClient>((client, _) => new TelegramBotClient(token, client));
             
             builder.Services.AddControllers();
             builder.Services.ConfigureTelegramBotMvc();
+            
             builder.Services.ConfigureTelegramBot<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt => opt.SerializerOptions);
+            
             
             var app = builder.Build();
 
+            
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
