bf8908a45b91b5f619987244e3b6eaeb4c452260
Unitazavr <grishazagidulin@list.ru>
2024-07-19 19:37:56 +0400

    Добавлен ID цветка в класс-объект

--- DataBase/FlowerObject.cs
+++ DataBase/FlowerObject.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore.Design;
+
 namespace FlowerSellerTgBot.DataBase;
 /**
  * Класс, описывающий объект-цветок и его методы
  */
 public class FlowerObject
 {
+    /**
+     * ID товара (цветка)
+     */
+    public int ProductId;
     /**
      * ID категории товара товара (цветка)
      */
@@ -32,9 +38,11 @@ public class FlowerObject
      * Цена товара (цветка)
      */
     public int Price; //Тут можно заменить на string, в теории(?)
+
     /// <summary>
     /// Конструктор объекта-цветка
     /// </summary>
+    /// <param name="productId">ID объекта</param>
     /// <param name="categoryId">ID категории</param>
     /// <param name="productName">Название</param>
     /// <param name="description">Описание</param>
@@ -42,8 +50,10 @@ public class FlowerObject
     /// <param name="price">Цена</param>
     /// <param name="photosId">ID фото (необязательно)</param>
     /// <param name="videosId">ID видео (необязательно)</param>
-    public FlowerObject(int categoryId, string productName, string description, int countOf, int price, int[]? photosId = null,  int? videosId = null)
+    public FlowerObject(int productId, int categoryId, string productName, string description, int countOf, int price,
+     int[]? photosId = null, int? videosId = null)
     {
+         ProductId = productId;
          CategoryId = categoryId;
          PhotosId = photosId;
          VideosId = videosId;
