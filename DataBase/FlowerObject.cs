5f2bfd8c914047ca830d344d068534b937444a4b
Unitazavr <grishazagidulin@list.ru>
2024-07-23 04:00:14 +0400

    Отлажен метод отправки медиа. Работает с видео и несколькими фото.

--- DataBase/FlowerObject.cs
+++ DataBase/FlowerObject.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
 using Microsoft.EntityFrameworkCore.Design;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -6,108 +8,85 @@ using Telegram.Bot.Types.Enums;
 namespace FlowerSellerTgBot.DataBase;
 
 /**
- * Класс, описывающий объект-цветок и его методы
- */
+* Класс, описывающий объект-цветок и его методы
+*/
 public class FlowerObject
 {
- /**
-  * ID товара (цветка)
-  */
- public int? ProductId;
-
- /**
-  * ID категории товара товара (цветка)
-  */
- public int CategoryId;
-
- /**
-  * Массив медиафайлов (ключ-значение) с ID файла и Типом файла.
-  */
- public KeyValuePair<string, InputMediaType>[]? MediaFiles;
-
- /**
-  * Название товара товара (цветка)
-  */
- public string ProductName;
-
- /**
-  * Описание товара (цветка)
-  */
- public string Description;
-
- /**
-  * Количество товара (цветка) в наличии
-  */
- public int CountOf;
-
- /**
-  * Цена товара (цветка)
-  */
- public int Price; //Тут можно заменить на string, в теории(?)
-
- /// <summary>
- /// Конструктор объекта-цветка
- /// </summary>
- /// <param name="productId">ID объекта</param>
- /// <param name="categoryId">ID категории</param>
- /// <param name="productName">Название</param>
- /// <param name="description">Описание</param>
- /// <param name="countOf">Количество</param>
- /// <param name="price">Цена</param>
- /// <param name="mediaFiles">Массив Медиафайлов (telegram ID - Type). По умолчанию - null</param>>
- public FlowerObject(int? productId, int categoryId, string productName, string description, int countOf, int price,
-  KeyValuePair<string, InputMediaType>[]? mediaFiles = null)
- {
-     ProductId = productId;
-     CategoryId = categoryId;
-     ProductName = productName;
-     Description = description;
-     CountOf = countOf;
-     Price = price;
-     //Если передают медиафайлы - записываем только первые три
-     if (mediaFiles != null)
-     {
-      MediaFiles = new KeyValuePair<string, InputMediaType>[3];
-      for (int i = 0; i < 3; i++)
-      {
-       MediaFiles[i] = mediaFiles[i];
-      }
-     }
- }
- /// <summary>
- /// Метод отпраки объекта - медиафайлов и описания
- /// </summary>
- /// <param name="bot">Бот-отправитель</param>
- /// <param name="id">ID чата</param>
- public void Send(ITelegramBotClient bot, ChatId id)
- {
-     if (MediaFiles == null || MediaFiles.Length == 0) 
-         return;
-     List<IAlbumInputMedia> inputMedia = new();
-     if (MediaFiles[0].Value == InputMediaType.Photo)
-     {
-         InputMediaPhoto firstInputWithDescription = new InputMediaPhoto(MediaFiles[0].Key);
-         firstInputWithDescription.Caption = $"{ProductName} - {Price}\n" + $"{Description}";
-         inputMedia[0] = firstInputWithDescription;
-     }
-     else if (MediaFiles[0].Value == InputMediaType.Video)
-     {
-         InputMediaVideo firstInputWithDescription = new InputMediaVideo(MediaFiles[0].Key);
-         firstInputWithDescription.Caption = $"{ProductName} - {Price}\n" + $"{Description}";
-         inputMedia[0] = firstInputWithDescription;
-     }
-     for (var i = 1; i < 3; i++)
-     {
-         KeyValuePair<string, InputMediaType> keyValuePair = MediaFiles[i];
-         if (keyValuePair.Value == InputMediaType.Photo)
-             inputMedia[i] = new InputMediaPhoto(keyValuePair.Key);
-         else if (keyValuePair.Value == InputMediaType.Video)
-             inputMedia[i] = new InputMediaVideo(keyValuePair.Key);
-     }
-     bot.SendMediaGroupAsync(id, inputMedia);
- }
- //TODO: Убрать костыли при добавлении загаловка первому элементу Inputmedia. Протестировать работу! 
- //TODO: Придумать, как выходить из метода при отсутствии медиа. (Exception какой-то, я полагаю)
-
- //Тимофей(funa4i) - На каждый цветок минимум одна фотка, по другому быть не может. Ошибку выбрасывать самое то!
+    /**
+     * Название категории товара
+     */
+    public string? CategoryName;
+    /**
+    * ID чата
+    */
+    public string? ChatId;
+    /**
+     * Массив медиафайлов (ключ-значение) с ID файла и Типом файла.
+     */
+    public KeyValuePair<string, InputMediaType>[]? MediaFiles;
+    /**
+     * Название товара товара (цветка)
+     */
+    public string? ProductName;
+    /**
+     * Описание товара (цветка)
+     */
+    public string? Description;
+    /**
+     * Цена товара (цветка)
+     */
+    public string? Price;
+    /// <summary>
+    /// Конструктор (будет дезинтегрирован в скором времени)
+    /// </summary>
+    /// <param name="categoryName">Имя категории</param>
+    /// <param name="chatId">ID чата</param>
+    /// <param name="mediaFiles">Массив медиафайлов (пары ключ-значение из ID и типа медиафайла)</param>
+    /// <param name="productName">Имя цветка</param>
+    /// <param name="description">Описание</param>
+    /// <param name="price">Цена</param>
+    public FlowerObject(string? categoryName, string? chatId, KeyValuePair<string, InputMediaType>[]? mediaFiles, string? productName, string? description, string? price)
+    {
+        CategoryName = categoryName;
+        ChatId = chatId;
+        MediaFiles = mediaFiles;
+        ProductName = productName;
+        Description = description;
+        Price = price;
+    }
+    /// <summary>
+    /// Метод отпраки объекта - медиафайлов и описания
+    /// </summary>
+    /// <param name="bot">Бот-отправитель</param>
+    /// <param name="id">ID чата</param>
+    public async Task Send(ITelegramBotClient bot, ChatId id)
+    {
+        if (MediaFiles == null || MediaFiles.Length == 0)
+            throw new NullReferenceException("Отсутствуют медиафайлы");
+        List<IAlbumInputMedia> inputMedia = new List<IAlbumInputMedia>(3);
+        if (MediaFiles[0].Value == InputMediaType.Photo)
+        {
+            var firstInputWithDescription = new InputMediaPhoto(MediaFiles[0].Key);
+            firstInputWithDescription.Caption = $"{ProductName} - {Price}\n" + $"{Description}";
+            inputMedia.Add(firstInputWithDescription);
+        }
+        else if (MediaFiles[0].Value == InputMediaType.Video)
+        {
+            var firstInputWithDescription = new InputMediaVideo(MediaFiles[0].Key);
+            firstInputWithDescription.Caption = $"{ProductName} - {Price}\n" + $"{Description}";
+            inputMedia.Add(firstInputWithDescription);
+        }
+        
+        for (int i = 1; i < (MediaFiles.Length < 3 ? MediaFiles.Length : 3); i++)
+        {
+            KeyValuePair<string, InputMediaType> keyValuePair = MediaFiles[i];
+            if (keyValuePair.Value == InputMediaType.Photo)
+                inputMedia.Add(new InputMediaPhoto(keyValuePair.Key));
+            else if (keyValuePair.Value == InputMediaType.Video)
+                inputMedia.Add(new InputMediaVideo(keyValuePair.Key));
+        }
+        await bot.SendMediaGroupAsync(id, inputMedia);
+    }
+    //TODO: Убрать костыли при добавлении загаловка первому элементу Inputmedia. Протестировать работу! 
+    //TODO: Сделать пустой конструктор/добавить сеттер к параметру InputMedia.
 }
